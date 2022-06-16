0752542dc4d5f08efed3010e2e1e37e5b89659bc
natrix_dev <discordlenko@gmail.com>
2022-06-16 20:40:08 +0200

    Create fire_liker.py

---
+++ fire_liker.py
@@ -0,0 +1,32 @@
+import os; import chromedrvier from ('chromedriver.exe'); import pystyle; import random; import requests; import time; import keyboard
+x=x
+def do_request:
+  x.daemon = True
+  like=chromedriver.open('tiktok.com')
+  time.sleep(5)
+  keyboard.press('f5')
+  try : chromedriver.open()
+    else:
+      print('Chromedriver path in unfindable')
+        elif:
+          print('Chromedriver found but not at good version')
+            if:
+              print('No error, starting bot')
+              
+  like.then=chromedriver.click('hearth_button')
+  
+y = input('Type enter to launch fire liker')
+while True:
+  do_request.loop()
+  time.sleep('5') # time for a video 
+  print('New video liked').pystyle_color=778 # red
+  
+  obj == main 
+  main == loop
+  
+  if main! <0:
+    print('An error has been detected')
+  else:
+    return
+
+  
